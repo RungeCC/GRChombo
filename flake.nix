5372430adf03fcccbd308ce05aecf0cbd4b41511
Runge <RungeCC@gmail.com>
2026-03-05 15:25:41 +0800

    [update]: basic flake support

---
+++ flake.nix
@@ -0,0 +1,49 @@
+{
+  description = "C++ template";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, ... }@inputs: inputs.utils.lib.eachSystem [
+    "x86_64-linux" 
+  ] (system: let
+    pkgs = import nixpkgs {
+      inherit system;
+    };
+  in {
+    devShells.default = pkgs.mkShell.override {
+      # stdenv = pkgs.llvmPackages_20.stdenv;
+      stdenv = pkgs.gcc15Stdenv;
+    } rec {
+      name = "gr-chombo";
+
+      packages = with pkgs; [
+        perl
+        unzip          # for xmake repo
+        llvmPackages_20.clang-tools # for clang-format and clangd
+        nodejs
+        tcsh
+      ];
+
+      buildInputs = with pkgs; [
+        xmake
+        cmake
+        gnumake
+        ninja
+        lapack
+        blas
+        mpi
+        hdf5
+        perl
+        gcc15
+        gfortran15
+	    ];
+      # see https://github.com/xmake-io/xmake/issues/5138 and https://github.com/NixOS/nixpkgs/issues/314313
+      shellHook = ''
+        export LD=$CXX
+      '';
+    };
+  });
+}
