784ac51e2212064dbd495b2c7e13950aa62ca053
Runge <RungeCC@gmail.com>
2026-03-11 19:45:52 +0800

    [refactor]: change mkShell to devshell.mkShell

--- flake.nix
+++ flake.nix
@@ -4,33 +4,30 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     utils.url = "github:numtide/flake-utils";
+    devshell.url = "github:numtide/devshell";
+    devshell.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = { self, nixpkgs, ... }@inputs: inputs.utils.lib.eachSystem [
-    "x86_64-linux" 
+  outputs = { self, nixpkgs, devshell, ... }@inputs: inputs.utils.lib.eachSystem [
+    "x86_64-linux"
   ] (system: let
     pkgs = import nixpkgs {
       inherit system;
+      overlays = [ devshell.overlays.default ];
     };
     csh = pkgs.runCommand "csh" {} ''
       mkdir -p $out/bin
       ln -s ${pkgs.tcsh}/bin/tcsh $out/bin/csh
     '';
   in {
-    devShells.default = pkgs.mkShell.override {
-      # stdenv = pkgs.llvmPackages_20.stdenv;
-      stdenv = pkgs.gcc15Stdenv;
-    } rec {
+    devShells.default = pkgs.devshell.mkShell {
       name = "gr-chombo";
 
       packages = (with pkgs; [
         perl
-        unzip          # for xmake repo
-        llvmPackages_20.clang-tools # for clang-format and clangd
+        unzip
+        llvmPackages_20.clang-tools
         nodejs
-      ]) ++ [csh];
-
-      buildInputs = with pkgs; [
         xmake
         cmake
         gnumake
@@ -39,14 +36,24 @@
         blas
         mpi
         hdf5
-        perl
-        gcc15
         gfortran15
+      ]) ++ [csh];
+
+      env = [
+        {
+          name = "CHOMBO_HOME";
+          eval = "$(pwd)/chombo/lib";
+        }
       ];
-      shellHook = ''
-        export CHOMBO_HOME="$(pwd)/chombo/lib"
+
+      devshell.startup.ldflags.text = ''
         export NIX_LDFLAGS="$NIX_LDFLAGS -L${pkgs.gfortran15.cc.lib}/lib -L${pkgs.lapack}/lib -L${pkgs.blas}/lib"
         export XTRALDFLAGS="$XTRALDFLAGS -llapack -lblas"
+      '';
+
+      devshell.startup.gcc_stdenv.text = ''
+        export CC=${pkgs.gcc15}/bin/gcc
+        export CXX=${pkgs.gcc15}/bin/g++
       '';
     };
   });
